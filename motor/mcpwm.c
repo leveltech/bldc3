75b9ac24787d431cf91c9bbf9788efa55b173e05
leveltech <austindmarhold@gmail.com>
2023-04-01 17:13:40 -0700

    a7

--- motor/mcpwm.c
+++ motor/mcpwm.c
@@ -159,6 +159,7 @@ static void pll_run(float phase, float dt, volatile float *phase_var,
 
 // Defines
 #define IS_DETECTING()			(state == MC_STATE_DETECTING)
+#define DEADTIME_CYCLES 200
 
 // Threads
 static THD_WORKING_AREA(timer_thread_wa, 512);
@@ -2694,52 +2695,73 @@ static void set_switching_frequency(float frequency) {
 	set_next_timer_settings(&timer_tmp);
 }
 
-static bool invert_duty_cycle = false;
+// Helper function to introduce deadtime
+static void delay_deadtime(void) {
+    volatile uint32_t deadtime_cycles = DEADTIME_CYCLES;
+    while (deadtime_cycles--) {
+        __NOP();
+    }
+}
 
 static void set_next_comm_step(int next_step) {
-	if (conf->motor_type == MOTOR_TYPE_DC) {
-		invert_duty_cycle = !invert_duty_cycle;
-		uint32_t pwm_mode = invert_duty_cycle ? TIM_OCMode_Inactive : TIM_OCMode_PWM1;
-		uint32_t inactive_mode = invert_duty_cycle ? TIM_OCMode_PWM1 : TIM_OCMode_Inactive;
+    static bool invert_duty_cycle = false;
 
-		// Deadtime
-		uint16_t deadtime = conf->dead_time;
+    if (conf->motor_type == MOTOR_TYPE_DC) {
+        invert_duty_cycle = !invert_duty_cycle;
+        delay_deadtime(); // Introduce deadtime before switching
 
-		// 0
-		TIM_SelectOCxM(TIM1, TIM_Channel_2, TIM_OCMode_Inactive);
-		TIM_CCxCmd(TIM1, TIM_Channel_2, TIM_CCx_Enable);
-		TIM_CCxNCmd(TIM1, TIM_Channel_2, TIM_CCxN_Disable);
+        // 0
+        TIM_SelectOCxM(TIM1, TIM_Channel_2, TIM_OCMode_Inactive);
+        TIM_CCxCmd(TIM1, TIM_Channel_2, TIM_CCx_Enable);
+        TIM_CCxNCmd(TIM1, TIM_Channel_2, TIM_CCxN_Disable);
 
-		if (direction) {
-			// +
-			TIM_SelectOCxM(TIM1, TIM_Channel_1, pwm_mode);
-			TIM_CCxCmd(TIM1, TIM_Channel_1, TIM_CCx_Enable);
-			TIM_CCxNCmd(TIM1, TIM_Channel_1, TIM_CCxN_Enable);
+        if (invert_duty_cycle) {
+            if (direction) {
+                // +
+                TIM_SelectOCxM(TIM1, TIM_Channel_1, TIM_OCMode_PWM1);
+                TIM_CCxCmd(TIM1, TIM_Channel_1, TIM_CCx_Enable);
+                TIM_CCxNCmd(TIM1, TIM_Channel_1, TIM_CCxN_Enable);
 
-			// -
-			TIM_SelectOCxM(TIM1, TIM_Channel_3, inactive_mode);
-			TIM_CCxCmd(TIM1, TIM_Channel_3, TIM_CCx_Enable);
-			TIM_CCxNCmd(TIM1, TIM_Channel_3, TIM_CCxN_Enable);
+                // -
+                TIM_SelectOCxM(TIM1, TIM_Channel_3, TIM_OCMode_Inactive);
+                TIM_CCxCmd(TIM1, TIM_Channel_3, TIM_CCx_Enable);
+                TIM_CCxNCmd(TIM1, TIM_Channel_3, TIM_CCxN_Enable);
+            } else {
+                // +
+                TIM_SelectOCxM(TIM1, TIM_Channel_3, TIM_OCMode_PWM1);
+                TIM_CCxCmd(TIM1, TIM_Channel_3, TIM_CCx_Enable);
+                TIM_CCxNCmd(TIM1, TIM_Channel_3, TIM_CCxN_Enable);
 
-			// Set deadtime
-			TIM1->BDTR |= (deadtime << 1);
-		} else {
-			// +
-			TIM_SelectOCxM(TIM1, TIM_Channel_3, pwm_mode);
-			TIM_CCxCmd(TIM1, TIM_Channel_3, TIM_CCx_Enable);
-			TIM_CCxNCmd(TIM1, TIM_Channel_3, TIM_CCxN_Enable);
+                // -
+                TIM_SelectOCxM(TIM1, TIM_Channel_1, TIM_OCMode_Inactive);
+                TIM_CCxCmd(TIM1, TIM_Channel_1, TIM_CCx_Enable);
+                TIM_CCxNCmd(TIM1, TIM_Channel_1, TIM_CCxN_Enable);
+            }
+        } else {
+            if (direction) {
+                // +
+                TIM_SelectOCxM(TIM1, TIM_Channel_3, TIM_OCMode_PWM1);
+                TIM_CCxCmd(TIM1, TIM_Channel_3, TIM_CCx_Enable);
+                TIM_CCxNCmd(TIM1, TIM_Channel_3, TIM_CCxN_Enable);
 
-			// -
-			TIM_SelectOCxM(TIM1, TIM_Channel_1, inactive_mode);
-			TIM_CCxCmd(TIM1, TIM_Channel_1, TIM_CCx_Enable);
-			TIM_CCxNCmd(TIM1, TIM_Channel_1, TIM_CCxN_Enable);
+                // -
+                TIM_SelectOCxM(TIM1, TIM_Channel_1, TIM_OCMode_Inactive);
+                TIM_CCxCmd(TIM1, TIM_Channel_1, TIM_CCx_Enable);
+                TIM_CCxNCmd(TIM1, TIM_Channel_1, TIM_CCxN_Enable);
+            } else {
+                // +
+                TIM_SelectOCxM(TIM1, TIM_Channel_1, TIM_OCMode_PWM1);
+                TIM_CCxCmd(TIM1, TIM_Channel_1, TIM_CCx_Enable);
+                TIM_CCxNCmd(TIM1, TIM_Channel_1, TIM_CCxN_Enable);
 
-			// Set deadtime
-			TIM1->BDTR |= (deadtime << 1);
-		}
-
-		return;
-	}
+                // -
+                TIM_SelectOCxM(TIM1, TIM_Channel_3, TIM_OCMode_Inactive);
+                TIM_CCxCmd(TIM1, TIM_Channel_3, TIM_CCx_Enable);
+                TIM_CCxNCmd(TIM1, TIM_Channel_3, TIM_CCxN_Enable);
+					}
+				}
+    return;
+}
 
 	uint16_t positive_oc_mode = TIM_OCMode_PWM1;
 	uint16_t negative_oc_mode = TIM_OCMode_Inactive;
